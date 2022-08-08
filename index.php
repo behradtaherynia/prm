a9743ff6a85033348ff5f6b007cd61df39dcdd36
Ali Ghanati <ali.ghanati90@yahoo.com>
2022-08-08 11:59:09 +0430

    added style.css and index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+get_header();
+
+
+get_footer();
+
+//class Person{
+//    private string $name;
+//    private int $age;
+//
+//    /**
+//     * @param string $name
+//     * @param int $age
+//     */
+//    public function __construct(string $name, int $age)
+//    {
+//        $this->name = $name;
+//        $this->age = $age;
+//    }
+//
+//    /**
+//     * @return string
+//     */
+//    public function getName(): string
+//    {
+//        return $this->name;
+//    }
+//
+//    /**
+//     * @return int
+//     */
+//    public function getAge(): int
+//    {
+//        return $this->age;
+//    }
+//}
+//class Student extends Person {
+//
+//    public function __construct(string $name, int $age)
+//    {
+//        parent::__construct($name,$age);
+//    }
+//    public function getAge(): int
+//    {
+//        return 20;
+//    }
+//}
+//$a=new Person('ali',12);
+//$b=new Student('ali',12);
+//echo $a->getAge();
+//echo $b->getAge();
