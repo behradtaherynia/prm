16e865d6693ee2577c4d6eb45e3fe1dd0b241527
Ali Ghanati <ali.ghanati90@yahoo.com>
2022-08-09 16:54:33 +0430

    merged WPCustomPostType.php with tadbira WPCustomePost.php

--- model/WPCustomPostType.php
+++ model/WPCustomPostType.php
@@ -1,6 +1,6 @@
 <?php
 
-abstract class WPCustomPostType
+abstract class WPCustomPostType extends WPClass
 {
     private int $ID;
 
@@ -77,11 +77,14 @@ abstract class WPCustomPostType
     }
 
     /**
+     * @param bool $editMode
      * @return string
      */
-    public function getContent(): string
+    public function getContent(bool $editMode = true): string
     {
-        return get_the_content(null, false, $this->getID());
+        $result = get_the_content(null, false, $this->getID());
+        return $editMode ? $result : apply_filters('the_content', $result);
+//        return wp_strip_all_tags();
     }
 
     /**
@@ -93,12 +96,11 @@ abstract class WPCustomPostType
     }
 
     /**
-     * @return string
+     * @return SmartDate
      */
-    public function getPublishDate(): string
+    public function getDate(): SmartDate
     {
-        $postDate = get_the_date('', $this->getID());
-        return $postDate == false ? false : $postDate;
+        return new SmartDate(get_post_time('U', false, $this->getID()));
     }
 
     /**
@@ -111,12 +113,12 @@ abstract class WPCustomPostType
     }
 
     /**
-     * @return false|string
+     * @return string
      */
-    public function getPostEditLink()
+    public function getEditLink(): string
     {
         $editLink = get_edit_post_link($this->getID());
-        return $editLink == null ? false : $editLink;
+        return $editLink == null ? '' : $editLink;
     }
 
     /**
@@ -127,7 +129,38 @@ abstract class WPCustomPostType
         return get_the_post_thumbnail_url($this->getID());
     }
 
+    /**
+     * @param string $attachmentDBKeyName
+     * @return Attachment[]
+     */
+    protected function getAttachmentsBy(string $attachmentDBKeyName): array
+    {
+        $attachments = [];
+        $attachmentsIDArray = $this->getPostMeta($attachmentDBKeyName, 'array');
+        foreach ($attachmentsIDArray as $attachmentID) {
+            $attachments[] = new Attachment($attachmentID);
+        }
+        return $attachments;
+    }
+    /**
+     * @param string $key
+     * @param string $metaValueType
+     * @return array|int|string
+     */
+    protected function getPostMeta(string $key, string $metaValueType = 'string')
+    {
+        $metaValue = get_post_meta($this->getID(), $key, true);
 
+        if ($metaValueType == 'int')
+            return !$metaValue ? 0 : $metaValue;
+        elseif ($metaValueType == 'bool')
+            return $metaValue;
+        elseif ($metaValueType == 'array')
+            return !$metaValue ? [] : unserialize($metaValue);
+        else
+            return !$metaValue ? '' : $metaValue;
+
+    }
 //endregion
 
 //region update functions::
@@ -197,6 +230,69 @@ abstract class WPCustomPostType
 //endregion
 
 //region Static functions::
+    /**
+     * @param string $customPostType
+     * @param string $className
+     * @param array $metaQuery
+     * @param int $count
+     * @param SmartDate|null $from
+     * @param SmartDate|null $to
+     * @param string $orderBy
+     * @param string $order
+     * @return array
+     */
+    protected static function Get(string $customPostType, string $className, array $metaQuery = [], int $count = -1, SmartDate $from = null, SmartDate $to = null, string $orderBy = 'date', string $order = 'DESC'): array
+    {
+        if ($count < 1) $count = -1;
+        if ($from == null) {
+            $dateQuery['after'] = [];
+        } else {
+            $dateQuery['after'] = $from->getDateStringGregorian() . ' 00:00:00';
+        }
+        if ($to != null) {
+            $dateQuery['before'] = $to->getDateStringGregorian() . ' 23:59:00';
+        } else {
+            $dateQuery['before'] = [];
+
+        }
+        $args = array(
+            'post_type' => array($customPostType),
+            'posts_per_page' => $count,
+            'post_status' => array('publish', 'future'),
+            'orderby' => $orderBy,
+            'order' => $order,
+            'date_query' => array(
+                $dateQuery,
+                'inclusive' => true
+            ),
+            'meta_query' => $metaQuery,
+        );
+        $list = new WP_Query($args);
+        $listArray = $list->get_posts();
+        return self::ConvertToObjectList($className, $listArray);
+
+    }
+
+    /**
+     * @param string $customPostType
+     * @param string $className
+     * @return array
+     */
+    protected static function GetAll(string $customPostType, string $className): array
+    {
+        return WPCustomPostType::Get($customPostType, $className);
+
+    }
+
+//endregion
+// region Class Protected Functions::
+
+    protected function getSortablePropertiesList(): array
+    {
+        return [
+            'ID' => $this->getID(),
+        ];
+    }
 
 //endregion
 }
