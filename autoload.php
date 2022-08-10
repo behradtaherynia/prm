80a2ee865492232d42cd51c7a11787e9a6b91826
Ali Ghanati <ali.ghanati90@yahoo.com>
2022-08-10 21:36:27 +0430

    fixed new dynamic class include

--- autoload.php
+++ autoload.php
@@ -1,11 +1,12 @@
 <?php
-spl_autoload_register( function($classname) {
+spl_autoload_register(function ($classname) {
+    var_ex($classname);
 
-    $class     = str_replace( '\\', DIRECTORY_SEPARATOR, str_replace( '_', '-', strtolower($classname) ) );
-
-    $file_path = get_template_directory() . DIRECTORY_SEPARATOR . $class . '.php';
-
-    if ( file_exists( $file_path ) )
-        require_once $file_path;
-
-} );
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, str_replace('_', '-', strtolower($classname)));
+    $classes = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR . $class . '.php';
+    var_ex($classes);
+    if (file_exists($classes)) {
+        include_once $classes ;
+//        include_once get_template_directory() . '/model/' . $classes . '.php';
+    }
+});
--- functions.php
+++ functions.php
@@ -1,27 +1,12 @@
 <?php
-//include get_template_directory() . '/autoload.php';
-//$aa = new Solution();
-//spl_autoload_register( function($classname) {
-//
-//    $class      = str_replace( '\\', DIRECTORY_SEPARATOR, str_replace( '_', '-', strtolower($classname) ) );
-//    $classes    = dirname(__FILE__) .  DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $class . '.php';
-//
-//    if( file_exists($classes) ) {
-//        require_once( $classes );
-//    }
-//} );
-//
-//$theme = new Client(1);
-//Requests::autoloader('WPCustomPostType');
-//Requests::autoloader('Solution');
-//Requests::register_autoloader();
+
 function var_ex($data)
 {
     echo '<pre>' . var_export($data, true) . '</pre>';
 
 }
 
-//require_once get_template_directory() . '/autoload.php';
+require_once get_template_directory() . '/autoload.php';
 
 //spl_autoload_register('myAutoloader');
 //
@@ -32,18 +17,18 @@ function var_ex($data)
 //
 //    include $path.$className.'.php';
 //}
-include_once get_template_directory() . '/model/WPClass.php';
-include_once get_template_directory() . '/model/WPCustomPostType.php';
-include_once get_template_directory() . '/model/WPTerm.php';
-
-foreach (glob(get_template_directory() . '/functions/*.php') as $filename) {
-    include_once $filename;
-}
-foreach (glob(get_template_directory() . '/model/*.php') as $filename) {
-    include_once $filename;
-}
-foreach (glob(get_template_directory() . '/dashboard/*') as $filename) {
-    foreach (glob($filename . '/*.php') as $phpFile) {
-        include_once $phpFile;
-    }
-}
+//include_once get_template_directory() . '/model/WPClass.php';
+//include_once get_template_directory() . '/model/WPCustomPostType.php';
+//include_once get_template_directory() . '/model/WPTerm.php';
+//
+//foreach (glob(get_template_directory() . '/functions/*.php') as $filename) {
+//    include_once $filename;
+//}
+//foreach (glob(get_template_directory() . '/model/*.php') as $filename) {
+//    include_once $filename;
+//}
+//foreach (glob(get_template_directory() . '/dashboard/*') as $filename) {
+//    foreach (glob($filename . '/*.php') as $phpFile) {
+//        include_once $phpFile;
+//    }
+//}
